d5b2876da03ca1eae474c2a4c57734c479554992
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-06-05 17:45:12 +0200

    Update script.ts

--- src/browser/script.ts
+++ src/browser/script.ts
@@ -2,104 +2,69 @@ interface Window {
     electron: any;
 }
 
-let savedIFrames = {};
+let iframes = document.querySelectorAll("iframe");
 
-setDragZone();
+function createStyle(doc = document) {
+    if (doc.querySelector("#electron-style")) return;
 
-setInterval(() => {
-    const iframes = document.querySelectorAll("iframe");
+    const style = doc.createElement("style");
 
-    iframes.forEach((iframe) => {
-        if (iframe && !savedIFrames[iframe.id]) {
-            savedIFrames[iframe.id] = {
-                iframe,
-                hidden: false
-            };
+    style.id = "electron-style";
+    style.innerHTML = document.querySelector(
+        "#electron-style-template"
+    ).innerHTML;
 
-            new MutationObserver(() => {
-                function checkOpensOtherApp(exclude: HTMLIFrameElement) {
-                    let isHidden = 0;
-
-                    iframes.forEach((iframe) => {
-                        if (
-                            iframe !== exclude &&
-                            iframe.classList.contains("view-hidden")
-                        )
-                            isHidden++;
-                    });
-
-                    console.log(isHidden, iframes.length);
-
-                    return (isHidden = iframes.length - 1);
-                }
-
-                const hidden = iframe.classList.contains("view-hidden"),
-                    opensOtherApp = checkOpensOtherApp(iframe);
-
-                console.log("opensOtherApp", opensOtherApp);
-
-                if (hidden !== savedIFrames[iframe.id].hidden) {
-                    if (hidden) window.electron.setIcon("drive");
-                    else {
-                        window.electron.setIcon(
-                            iframe.id
-                                .replace(/[\d-]/g, "")
-                                .replace("iclouddrive", "drive")
-                        );
-                    }
-
-                    savedIFrames[iframe.id].hidden = hidden;
-                }
-            }).observe(iframe, {
-                attributes: true,
-                attributeFilter: ["class"]
-            });
-
-            setDragZone(iframe.contentDocument, true);
-
-            window.electron.setIcon(
-                iframe.id.replace(/[\d-]/g, "").replace("iclouddrive", "drive")
-            );
-            iframe.addEventListener("load", () =>
-                setDragZone(iframe.contentDocument, true)
-            );
-        }
-    });
-}, 1000);
-
-function setDragZone(doc = document, isApp = false) {
-    let clear = 0,
-        toolbar = doc.querySelector<HTMLDivElement>(".toolbar-view"),
-        username = doc.querySelector<HTMLDivElement>(".user-name-with-chevron"),
-        appSelect = doc.querySelector<HTMLDivElement>(
-            ".application-toolbar-left-view"
-        );
-
-    const interval = setInterval(() => {
-        toolbar = doc.querySelector(".toolbar-view");
-        username = doc.querySelector(".user-name-with-chevron");
-        if (isApp)
-            appSelect = doc.querySelector(".application-toolbar-left-view");
-
-        if (toolbar) {
-            clear++;
-            // @ts-ignore
-            toolbar.style.webkitAppRegion = "drag";
-        }
-
-        if (username) {
-            clear++;
-            // @ts-ignore
-            username.style.webkitAppRegion = "no-drag";
-        }
-
-        if (isApp && appSelect) {
-            clear++;
-            // @ts-ignore
-            appSelect.style.webkitAppRegion = "no-drag";
-        }
-
-        if ((!isApp && clear >= 2) || (isApp && clear >= 3))
-            clearTimeout(interval);
-    }, 100);
+    doc.querySelector("head").appendChild(style);
 }
+
+function writeTextToInput(input, text) {
+    input.value = text;
+    input.dispatchEvent(new Event("input"));
+}
+
+async function createPasswordListener(element) {
+    const password = await window.electron.getPassword();
+
+    setTimeout(() => {
+        writeTextToInput(element, password);
+    }, 1000);
+
+    element.addEventListener("input", (e) => {
+        window.electron.setPassword(e.target.value);
+    });
+}
+
+new MutationObserver(() => {
+    console.log(
+        "mutate",
+        document.querySelectorAll("iframe").length,
+        iframes.length
+    );
+    if (document.querySelectorAll("iframe").length !== iframes.length) {
+        iframes = document.querySelectorAll("iframe");
+
+        iframes.forEach((iframe) => {
+            iframe.addEventListener("load", () => {
+                const doc = iframe.contentDocument,
+                    listeners = [];
+
+                createStyle(doc);
+
+                new MutationObserver(() => {
+                    const password = doc.querySelector("input[type=password]");
+
+                    if (password && !listeners.includes(password)) {
+                        createPasswordListener(password);
+                        listeners.push(password);
+                    }
+                }).observe(doc, {
+                    childList: true,
+                    subtree: true
+                });
+            });
+        });
+    }
+}).observe(document.body, {
+    childList: true,
+    subtree: true
+});
